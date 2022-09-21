c867f69abb9d370a9d2cc3a0f0c7a40c120b02e4
sina <sinaolfati5@gmail.com>
2022-09-21 15:19:42 +0430

    Join: styled header and hard-way

--- src/Join.js
+++ src/Join.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Join.css'
 
+// images
+import facebook from './assets/images/facebook.svg'
+import google from './assets/images/google.png'
+import apple from './assets/images/apple.svg'
+
 const Join = () => {
   return (
     <div className='join-container'>
       <div className='join-content'>
         <h1>Join Fiverr</h1>
         <div className='ready-ways'>
-            <button><logo>logo</logo><h2>Continue with Facebook</h2></button>
-            <button><logo>logo</logo><h2>Continue with Google</h2></button>
-            <button><logo>logo</logo><h2>Continue with Apple</h2></button>
+            <button><img src={facebook} /><h2>Continue with Facebook</h2></button>
+            <button><img src={google} /><h2>Continue with Google</h2></button>
+            <button><img src={apple} /><h2>Continue with Apple</h2></button>
+            <div className='or'>OR</div>
         </div>
         <div className='hard-way'>
             <input />
