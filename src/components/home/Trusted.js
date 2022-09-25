e758f1b914472c556c13ca029f89545f429afcde
sina <sinaolfati5@gmail.com>
2022-09-25 21:37:01 +0330

    Trusted.js: completed

--- src/components/home/Trusted.js
+++ src/components/home/Trusted.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Trusted.css'
 
+// images
+import facebook from '../../assets/images/facebook.png'
+import google2 from '../../assets/images/google2.png'
+import netflix from '../../assets/images/netflix.png'
+import pandg from '../../assets/images/pandg.png'
+import paypal from '../../assets/images/paypal.png'
+
 const Trusted = () => {
   return (
     <div className='trusted-container'>
       <h2>Trusted by:</h2>
+      <img src={facebook} alt='logo'/>
+      <img src={google2} alt='logo'/>
+      <img src={netflix} alt='logo'/>
+      <img src={pandg} alt='logo'/>
+      <img src={paypal} alt='logo'/>
     </div>
   )
 }
