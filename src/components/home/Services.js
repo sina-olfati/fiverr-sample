8f0ffece14a34e2a54689d15ca20a6b5c8a1b3fa
sina <sinaolfati5@gmail.com>
2022-09-25 23:00:11 +0330

    made the functionality

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Services.css'
 
 const Services = () => {
+
+    const [slider, setSlider] = useState(1)
+
+
   return (
+    <div className='services'>
     <div className='services-container'>
       <h1>Popular professional services</h1>
-      <div className='slider'>
+      <div className={`slider ${slider == 1 ? 'first' : 'second'}`}>
 
         <div className='one'>
             <div className='item'>
@@ -45,14 +50,14 @@ const Services = () => {
         <div className='item'>
                 <img src='' />
                 <div className='texts'>
-                    <p>Build your brand</p>
-                    <h2>Logo Design</h2>
+                    <p>Build your brand2</p>
+                    <h2>Logo Design2</h2>
                 </div>
             </div>
             <div className='item'>
                 <div className='texts'>
-                    <p>Customize your site</p>
-                    <h2>WordPress</h2>
+                    <p>Customize your site2</p>
+                    <h2>WordPress2</h2>
                 </div>
             </div>
             <div className='item'>
@@ -75,10 +80,10 @@ const Services = () => {
             </div>
         </div>
 
-        <button className='right-button'></button>
-        <button className='left-button'></button>
-
       </div>
+    </div>
+    <button className='right-button' onClick={() => slider < 2 ? setSlider(slider + 1) : null}>+</button>
+    <button className='left-button' onClick={() => slider > 1 ? setSlider(slider - 1) : null}>-</button>
     </div>
   )
 }
