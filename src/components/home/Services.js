3588bafeca1faa25a69d3a435dc8147106396494
sina <sinaolfati5@gmail.com>
2022-09-27 21:51:22 +0330

    Services: added images and total styling

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 import './Services.css'
 
+// images
+import a1 from '../../assets/images/a1.png'
+import a2 from '../../assets/images/a2.png'
+import a3 from '../../assets/images/a3.png'
+import a4 from '../../assets/images/a4.png'
+import a5 from '../../assets/images/a5.png'
+import a6 from '../../assets/images/a6.png'
+import a7 from '../../assets/images/a7.png'
+import a8 from '../../assets/images/a8.png'
+import a9 from '../../assets/images/a9.png'
+import a10 from '../../assets/images/a10.png'
+import arrow from '../../assets/images/arrow.png'
+
 const Services = () => {
 
     const [slider, setSlider] = useState(1)
@@ -14,31 +27,35 @@ const Services = () => {
 
         <div className='one'>
             <div className='item'>
-                <img src='' />
+                <img src={a1} />
                 <div className='texts'>
                     <p>Build your brand</p>
                     <h2>Logo Design</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a2} />
                 <div className='texts'>
                     <p>Customize your site</p>
                     <h2>WordPress</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a3} />
                 <div className='texts'>
                     <p>Share your message</p>
                     <h2>Voice Over</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a4} />
                 <div className='texts'>
                     <p>Engage your audience</p>
                     <h2>Video Explainer</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a5} />
                 <div className='texts'>
                     <p>Reach more customers</p>
                     <h2>Social Media</h2>
@@ -48,42 +65,46 @@ const Services = () => {
 
         <div className='two'>
         <div className='item'>
-                <img src='' />
+                <img src={a6} />
                 <div className='texts'>
-                    <p>Build your brand2</p>
-                    <h2>Logo Design2</h2>
+                    <p>Unlock growth online</p>
+                    <h2>SEO</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a7} />
                 <div className='texts'>
-                    <p>Customize your site2</p>
-                    <h2>WordPress2</h2>
+                    <p>Color your dreams</p>
+                    <h2>Illustration</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a8} />
                 <div className='texts'>
-                    <p>Share your message</p>
-                    <h2>Voice Over</h2>
+                    <p>Go global</p>
+                    <h2>Translation</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a9} />
                 <div className='texts'>
-                    <p>Engage your audience</p>
-                    <h2>Video Explainer</h2>
+                    <p>Learn your business</p>
+                    <h2>Data Entry</h2>
                 </div>
             </div>
             <div className='item'>
+            <img src={a10} />
                 <div className='texts'>
-                    <p>Reach more customers</p>
-                    <h2>Social Media</h2>
+                    <p>Showcase your story</p>
+                    <h2>Book Covers</h2>
                 </div>
             </div>
         </div>
 
       </div>
     </div>
-    <button className='right-button' onClick={() => slider < 2 ? setSlider(slider + 1) : null}> + </button>
-    <button className='left-button' onClick={() => slider > 1 ? setSlider(slider - 1) : null}> - </button>
+    <button className='right-button' onClick={() => slider < 2 ? setSlider(slider + 1) : null}><img src={arrow} /></button>
+    <button className='left-button' onClick={() => slider > 1 ? setSlider(slider - 1) : null}><img src={arrow} /></button>
     </div>
   )
 }
